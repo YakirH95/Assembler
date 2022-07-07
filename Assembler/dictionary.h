41df2ee9572d090f9a85bec2d5135b4e8e437390
Yakir <yakirh95@gmail.com>
2022-07-07 23:23:38 +0300

    added functions sygnatures

--- Assembler/dictionary.h
+++ Assembler/dictionary.h
@@ -1,2 +1,6 @@
+#pragma once
 
-char* search_key(struct entry);
+dictionary* create_dictionary();
+void delete_dictionary(dictionary* d);
+void add_entry(dictionary* d, char* k, char* v);
+char* get_value(dictionary* d, char* key);
