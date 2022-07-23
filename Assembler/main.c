b46e60e894546c51f9aeeddc7e6154e8ccd0e517
Yakir <yakirh95@gmail.com>
2022-07-23 13:24:18 +0300

    added notes, created first_process module

--- Assembler/main.c
+++ Assembler/main.c
@@ -14,21 +14,26 @@ int main(int argc, char* argv[])
 
 	char* buffer = 0;
 	int length;
+	//Open file given as paramater at argv[1]
 	FILE* file = fopen(argv[1], "rb");
 	if (file == NULL)
 	{
 		printf("File not exist");
 	}
 
+	//Point to end of file
 	fseek(file, 0, SEEK_END);
+	//Get file size
 	length = ftell(file);
+	//Point to beggining of file
 	fseek(file, 0, SEEK_SET);
+	//Allocate memory for file
 	buffer = malloc(length);
+	//Read file to buffer
 	fread(buffer, 1, length, file);
 	fclose(file);
 
 	char* expanded_output = expand_macro(buffer);
 	
-
 	return 0;
 }
